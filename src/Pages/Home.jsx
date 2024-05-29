70546b30b48af20af8cc2fc18c33fdf297fffe6f
Deji Arib <114099878+deji2ghost@users.noreply.github.com>
2024-05-29 16:32:33 +0100

    update: hieight hot-fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,7 +13,7 @@ export const Home = ({ currentUser }) => {
         setCollapsed(!collapsed)
     }
   return (
-    <div className='app text-stone-100 flex h-[1000px] gap-2'>
+    <div className='app text-stone-100 flex h-[100%] gap-2'>
         {/* sideNav */}
         <div className={`bg-black ${collapsed ? 'transform w-[23%]' : 'transform w-[7%]'} transition-all ease-in-out duration-300 p-2 flex flex-col gap-5 h-screen sticky top-0 drop-shadow-lg`}>
             <button onClick={handleCollapse} className='pt-3 transition-all ease-in-out'>{collapsed ? <FontAwesomeIcon icon={faChevronLeft} /> : <FontAwesomeIcon icon={faChevronRight} /> }</button>
