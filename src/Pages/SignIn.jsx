8f5d2b23fccc4b212359e2039a8ee4b373aaa148
Deji Arib <114099878+deji2ghost@users.noreply.github.com>
2024-05-29 13:57:18 +0100

    update to ui

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -3,6 +3,7 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { Link } from 'react-router-dom'
+import './SignIn.css'
 
 export const SignIn = ({ signinUser }) => {
 
@@ -29,31 +30,48 @@ export const SignIn = ({ signinUser }) => {
     }
 
   return (
-    <form 
-        onSubmit={handleSubmit(onSubmit, onError)} 
-        className='w-[20%] mx-auto bg-white flex flex-col gap-3 p-3 justify-center items-center'
-    >
-        <h1 className=''>Sign In</h1>
-        <div className='flex flex-col w-[50%]'>
-            <label>Email</label>
-            <input 
-                type='text'
-                placeholder='enter your email here'
-                {...register('email')}
-                className='border-b outline-none px-5 py-1'
-            /> 
+    <div className='main flex justify-center align-auto h-[100vh] pt-20 pb-20 pr-20 pl-20'>
+        <div className='welcome-div flex justify-center align-center w-[60%] border border-black rounded-l-lg bg-black p-[30px]'>
+            <div className='logo px-6'>
+                <div className='flex justify-center items-center font-bold mt-40'>
+                    <h1 className='bg-blue-900 rounded-lg text-5xl font-medium px-1 mr-2'>A</h1>
+                    <p className='transition-all ease-in-out duration-500 font-medium text-white text-5xl'>ACCATEX</p>
+                </div>
+            </div>
         </div>
-        <div className='flex flex-col w-[50%]'>
-            <label>Password</label>
-            <input 
-                type='text'
-                placeholder='enter your password here'
-                {...register('password')}
-                className='border-b outline-none px-5 py-1'
-            /> 
+        <div className='form-div flex w-[40%] rounded-r-lg bg-white p-[35px]'>
+            <form
+                onSubmit={handleSubmit(onSubmit, onError)} 
+                className='w-[100%] mx-auto bg-white flex justify-center'
+            >
+                <div className='w-[100%]'>
+                    <p className='welcome-2 hidden'>Welcome back!</p>
+                    <h1 className='sign-in font-light text-3xl mb-[20px] mt-[80px]'>Sign In</h1>
+                    <div className='flex flex-col'>
+                        {/* <label>Email</label> */}
+                        <input 
+                            type='text'
+                            placeholder='email'
+                            {...register('email')}
+                            className='border-b border-gray-400 outline-none text-sm mb-[15px] py-2'
+                        /> 
+                    </div>
+                    <div className='flex flex-col'>
+                        {/* <label>Password</label> */}
+                        <input 
+                            type='text'
+                            placeholder='password'
+                            {...register('password')}
+                            className='border-b border-gray-400 outline-none text-sm mb-[30px] py-2'
+                        /> 
+                    </div>
+                    <div className='flex flex-col'>
+                        <button type='submit' className='bg-black text-white text-sm rounded-full mb-[20px] pt-[5px] pb-[5px] pr-[7px] pl-[7px] hover:bg-slate-700 duration-300 transition-all ease-in-out'>sign in</button>
+                        <button><Link to='/SignUp' className='text-sm text-slate-500 hover:underline'>Dont have an account</Link></button>
+                    </div>
+                </div>
+            </form>
         </div>
-        <button type='submit' className='bg-purple-600 rounded-md p-3 w-[20%]'>Sign In</button>
-        <Link to='/SignUp'>Dont have an account</Link>
-    </form>
+    </div>
   )
 }
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -29,39 +29,53 @@ export const SignUp = ({ registerUser }) => {
     }
 
   return (
-    <form 
-        onSubmit={handleSubmit(onSubmit, onError)} 
-        className='w-[20%] mx-auto bg-white flex flex-col gap-3 p-3 justify-center items-center'
-    >
-        <h1>Sign Up</h1>
-        <div className='flex flex-col w-[50%]'>
-            <label>Email</label>
-            <input 
-                type='text'
-                placeholder='enter your email here'
-                {...register('email')}
-                className='border-b outline-none px-5 py-1'
-            /> 
+    <div className='main flex justify-center align-auto h-[100vh] pt-20 pb-20 pr-20 pl-20'>
+        <div className='welcome-div flex justify-center align-center w-[60%] border border-black rounded-l-lg bg-black p-[30px]'>
+            <div className='logo px-6'>
+                <div className='flex justify-center items-center font-bold mt-40'>
+                    <h1 className='bg-blue-900 rounded-lg text-5xl font-medium px-1 mr-2'>A</h1>
+                    <p className='transition-all ease-in-out duration-500 font-medium text-white text-5xl'>ACCATEX</p>
+                </div>
+            </div>
         </div>
-        <div className='flex flex-col w-[50%]'>
-            <label>Password</label>
-            <input 
-                type='text'
-                placeholder='enter your password here'
-                {...register('password')}
-                className='border-b outline-none px-5 py-1'
-            /> 
+        <div className='form-div flex w-[40%] rounded-r-lg bg-white p-[35px]'>
+            <form 
+                onSubmit={handleSubmit(onSubmit, onError)} 
+                className='w-[100%] mx-auto bg-white flex justify-center'
+            >
+                <div className='w-[100%]'>
+                <p className='welcome-2 hidden'>Create your account</p>
+                    <h1 className='sign-in font-light text-3xl mb-[20px] mt-[80px]'>Sign Up</h1>
+                    <div className='flex flex-col'>
+                        <input 
+                            type='text'
+                            placeholder='email'
+                            {...register('email')}
+                            className='border-b border-gray-400 outline-none text-sm mb-[15px]  py-2'
+                        /> 
+                    </div>
+                    <div className='flex flex-col'>
+                        <input 
+                            type='text'
+                            placeholder='password'
+                            {...register('password')}
+                            className='border-b border-gray-400 outline-none text-sm mb-[15px]  py-2'
+                        /> 
+                    </div>
+                    <div className='flex flex-col'>
+                        <input 
+                            type='text'
+                            placeholder='confirm password'
+                            {...register('confirmPassword')}
+                            className='border-b border-gray-400 outline-none text-sm mb-[30px]  py-2'
+                        /> 
+                    </div>
+                    <div className='flex flex-col'>
+                        <button type='submit' className='bg-black text-white text-sm rounded-full mb-[20px] pt-[5px] pb-[5px] pr-[7px] pl-[7px] hover:bg-slate-700 duration-300 transition-all ease-in-out'>Sign Up</button>
+                    </div>
+                </div>
+            </form>
         </div>
-        <div className='flex flex-col w-[50%]'>
-            <label>Confirm Password</label>
-            <input 
-                type='text'
-                placeholder='confirm your password here'
-                {...register('confirmPassword')}
-                className='border-b outline-none px-5 py-1'
-            /> 
-        </div>
-        <button type='submit' className='bg-purple-600 rounded-md p-3 w-[20%]'>Sign Up</button>
-    </form>
+    </div>
   )
 }
