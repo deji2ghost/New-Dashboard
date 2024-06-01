b1cbc665dbfe84db3384f441ae2c4bb89051b025
deji2ghost <dejiwilliams9@gmail.com>
2024-06-01 14:06:56 +0200

    new commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import { SideNav } from '../Components/SideNav';
 import { Header } from '../Components/Header';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
+import { auth } from '../Auth/firebase-config';
 
 export const Home = ({ currentUser, setUserLoggedIn, userLoggedIn }) => {
 
@@ -32,7 +33,7 @@ export const Home = ({ currentUser, setUserLoggedIn, userLoggedIn }) => {
   return (
     <div className='app text-stone-100 flex h-[100%] gap-2'>
         {/* sideNav */}
-        <div className={`bg-black ${collapsed ? 'transform w-[23%]' : 'transform w-[7%]'} transition-all ease-in-out duration-300 p-2 flex flex-col gap-5 sticky top-0 drop-shadow-lg`}>
+        <div className={`bg-black ${collapsed ? 'transform w-[23%]' : 'transform w-[7%]'} transition-all ease-in-out duration-300 p-2 flex flex-col gap-5 h-screen sticky top-0 drop-shadow-lg`}>
             <button onClick={handleCollapse} className='pt-3 transition-all ease-in-out'>{collapsed ? <FontAwesomeIcon icon={faChevronLeft} /> : <FontAwesomeIcon icon={faChevronRight} /> }</button>
             <SideNav collapsed={collapsed} setUserLoggedIn={setUserLoggedIn} userLoggedIn={userLoggedIn} handleModal={handleModal}/>
         </div>
