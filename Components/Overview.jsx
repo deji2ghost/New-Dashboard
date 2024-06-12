22be4b99f6339fb233cf38a9d2f99fb4f5b4af82
Deji Arib <114099878+deji2ghost@users.noreply.github.com>
2024-06-12 20:34:43 +0100

    update: hover ease-in-out effect on download button

--- Components/Overview.jsx
+++ Components/Overview.jsx
@@ -51,7 +51,7 @@ export const Overview = (props) => {
             {/* <div className='react-calendar'>
               <Calendar value={currentDate} onChange={setCurrentDate} />
             </div> */}
-            <button className='bg-violet-950 px-3 py-1 mr-2 ml-3 rounded-sm hover:bg-purple-400'>
+            <button className='bg-violet-950 px-3 py-1 mr-2 ml-3 rounded-sm hover:bg-violet-500 transition ease-in-out duration-300'>
                 <FontAwesomeIcon icon={faDownload}/>
                 <span className='ml-2'>{t(`downloadReport`)}</span>
             </button>
